144cd514edb93957fb613cdc332ba39cf8ccc57b
leopoldkwok <leopoldkwok@gmail.com>
2023-12-19 13:48:59 +0000

    Token Actions

--- utils/action.js
+++ utils/action.js
@@ -1,6 +1,7 @@
 'use server';
 import OpenAI from 'openai';
 import prisma from './db';
+import { revalidatePath } from 'next/cache';
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -124,4 +125,45 @@ export const generateTourImage = async ({ city, country }) => {
   } catch (error) {
     return null;
   }
+};
+
+export const fetchUserTokensById = async (clerkId) => {
+  const result = await prisma.token.findUnique({
+    where: {
+      clerkId,
+    },
+  });
+  return result?.tokens;
+};
+
+export const generateUserTokensForId = async (clerkId) => {
+  const result = await prisma.token.create({
+    data: {
+      clerkId,
+    },
+  });
+  return result?.tokens;
+};
+
+export const fetchOrGenerateTokens = async (clerkId) => {
+  const result = await fetchUserTokensById(clerkId);
+  if (result) {
+    return result.tokens;
+  }
+  return await generateUserTokensForId(clerkId).tokens;
+};
+
+export const subtractTokens = async (clerkId, tokens) => {
+  const result = await prisma.token.update({
+    where: {
+      clerkId,
+    },
+    data: {
+      tokens: {
+        decrement: tokens,
+      },
+    },
+  });
+  revalidatePath('/profile');
+  return result.tokens;
 };
